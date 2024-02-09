56709305505d0421c45c7f188b95c184c5121d17
sondreaa <104071423+sondreaa@users.noreply.github.com>
2024-02-09 15:13:19 +0100

    d

---
+++ databaser/main.ts
@@ -0,0 +1,16 @@
+console.log("ajksd")
+
+class Elev {
+    constructor(
+        elevNr: number, 
+        navn: string, 
+        klasse: string,
+        interesse: number, 
+        kontaktlaererNr: number, 
+        telefonNr: number, 
+        termin1: number, 
+        standpunkt: number,
+        ) {
+      
+    }
+  }
